cdbc928039409d304ebe39f371902e07c9fa5cb0
Renaldy <mickael.kristianto@spectrumindo.com>
2025-05-21 20:22:54 +0700

    Fix bcrypt

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const prisma = require('../../database/connection');
 const { JWT_SECRET, JWT_TTL } = require('../../config');
